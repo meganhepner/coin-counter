46b45d529e3d2bf3b866595c81ef89003b3dec85
Kate Skorija <kateskorija@gmail.com>
2020-08-31 13:36:57 -0700

    solve 2, 3, 4 practice closure problems
    Co-authored-by: Megan Hepner <megan.hepner@gmail.com>

--- src/closurePractice.js
+++ src/closurePractice.js
@@ -9,3 +9,37 @@ const prefixDuchess = addPrefix("Duchess");
 
 console.log(prefixSir(" Johnny"));
 console.log(prefixDuchess(" Megan"));
+
+function soundMaker(noise) {
+  return function(animal) {
+    return `${animal} says ${noise}`
+  }
+}
+
+const lionSound = soundMaker("roar");
+const mouseSound = soundMaker("squeak");
+
+console.log(lionSound("Lion"));
+
+function addFix(prefix) {
+  return function(suffix) {
+    return function(name) {
+      return `${prefix}${name}${suffix}`
+    } 
+  }
+}
+
+const misterJunior = addFix("Mister")("Junior");
+
+console.log(misterJunior(" Johnny "));
+
+function addAndMult(add) {
+  return function(mul) {
+    return function(num) {
+      return `${num + add}, ${num * mul}`;
+    }
+  }
+}
+
+const addTwoMultiplyByThree = addAndMult(2)(3);
+console.log(addTwoMultiplyByThree(4));
